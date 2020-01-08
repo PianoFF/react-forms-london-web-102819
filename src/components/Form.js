a2edddd70afd7aec5bee861e2043f6b9961c8173
PianoFF <Fangfei1star@gmail.com>
2020-01-08 16:40:57 +0000

    onChange event and input attributes

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
-    firstName: "First Name",
-    lastName: "Last Name "
+    firstName: "",
+    lastName: ""
   }
 
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
+      /*
+      The event contains data about the target, which is whatever DOM element the event was triggered on. 
+      That target, being an input, has a value attribute. This attribute is equal to whatever is currently entered into that particular input!
+      Keep in mind, this is not the value we provided from state. When we read event.target.value, 
+      we get whatever content is present when the event fired. 
+      In the case of our first input, that would be a combination of whatever this.state.firstName is equal to plus the last key stroke. */
     })
   }
 
@@ -27,10 +33,10 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <lable htmlFor='first-name'>First Name </lable>
-        <input type="text" id='first-name' name="firstName" onChange={this.handleFirstNameChange} value={this.state.firstName} />
+        <input type="text" id='first-name' name="firstName" placeholder='first-name' onChange={this.handleFirstNameChange} value={this.state.firstName} />
         <br></br>
         <lable htmlFor='last-name'>Last Name </lable>               
-        <input type="text" id='last-name' name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input type="text" id='last-name' name="lastName" placeholder='last-name' onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
     )
   }
